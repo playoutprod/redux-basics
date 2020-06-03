e4d87c6beee143313d3e8f4c310c15e6d15564ad
playoutprod <35104962+playoutprod@users.noreply.github.com>
2020-06-04 00:08:30 +0200

    Phonebook as const for proptypes

--- src/components/Phonebook.js
+++ src/components/Phonebook.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default (props) =>{
+const Phonebook = (props) =>{
   return(
     <div>
       {
@@ -16,3 +16,4 @@ Phonebook.propTypes = {
 Phonebook.defaultProps = {
   users:[]
 }
+export default Phonebook;
